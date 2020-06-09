45b29b707031a3be351308d2d1e2e8dbd6d142a0
Shinjice <Tdna84@gmail.com>
2020-06-09 21:57:14 +0200

    added mapname validator

--- src/CodeWarsFileCreator.java
+++ src/CodeWarsFileCreator.java
@@ -1,6 +1,4 @@
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -8,34 +6,41 @@ import java.util.Scanner;
 public class CodeWarsFileCreator {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-        List<String> tokens = new ArrayList<>();
+        List<String> multiLine = new ArrayList<>();
 
         System.out.println("Enter KataName");
-        String mapName = sc.nextLine() + "\\";
+        String mapName = sc.nextLine().replace("/","")
+                                    .replace("\\","")
+                                    .replace(":","")
+                                    .replace("?","")
+                                    .replace("\"","")
+                                    .replace("<","")
+                                    .replace(">","")
+                                    .replace("|","")
+                                    .replace("*","") + "\\";
 
         System.out.println("Enter KataURL");
         String websiteKata = "\n//" + sc.nextLine();
 
         System.out.println("Enter KataCode and close with CTRL+D");
         while (sc.hasNext()) {
-            tokens.add(sc.nextLine());
+            multiLine.add(sc.nextLine());
         }
 
         File theDir = new File("C:\\Users\\Gebruiker\\Desktop\\CodeWars\\8kyu\\" + mapName);
         theDir.mkdir();
         FileWriter myWriter = new FileWriter("C:\\Users\\Gebruiker\\Desktop\\CodeWars\\8kyu\\" + mapName + "Demo.java");
-        myWriter.write(  loop(tokens)  + websiteKata);
+        myWriter.write(  loop(multiLine)  + websiteKata);
         myWriter.close();
         System.out.println("File created!");
+
     }
 
-    public static String loop(List<String> tokens){
+    public static String loop(List<String> multiLine){
         String temp = "";
-        for(int i = 0; i < tokens.size(); i++){
-            temp += tokens.get(i) + "\n";
+        for(int i = 0; i < multiLine.size(); i++){
+            temp += multiLine.get(i) + "\n";
         }
         return temp;
     }
-
-
 }
